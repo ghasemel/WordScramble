e22907d4a060650a557d694e37e83bf761fe0f12
Ghasem Elyasi <ghasem.elyasi@gmail.com>
2024-02-06 15:30:36 +0100

    reading random word from bundle file

---
+++ README.md
@@ -0,0 +1,9 @@
+# WordScramble
+
+This is an iOS sample app built based on [Day 29 – project 5](https://www.hackingwithswift.com/100/swiftui/29)
+
+Generate new words based on the letters peresent in the given word! 
+
+## Tools
+- Xcode: 15.2
+- iOS: 17
--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                TextField("Enter your word", text: $newWord)
+                    .textInputAutocapitalization(.never)
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
         }
-        .padding()
+        .onSubmit(addNewWord)
+        .onAppear(perform: startGame)
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    
+    func startGame() {
+        if let startWordURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
